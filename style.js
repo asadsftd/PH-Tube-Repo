ef1377bb8c34b8eee0e4be05b264a8c3c2a2f496
asadsftd <asadkhanopu6@gmail.com>
2026-04-19 16:39:37 -0700

    added the my dynamic videos loaded

--- style.js
+++ style.js
@@ -13,15 +13,52 @@ const dynamicButtonsLoad = (buttons)=>{
         const buttonsContainer = document.getElementById("dynamic-buttons");
         const div = document.createElement("div");
         div.innerHTML = `
-         <button class=" 
-         rounded-md btn btn-xs text-2xl ">${button.category}</button>
+         <button class=" hover:bg-[#FF1F3D] hover:text-white
+         rounded-md btn btn-xs text-2xl p-4 ">${button.category}</button>
         
         
         `;
         buttonsContainer.appendChild(div);
     }
 }
+// 
+    // dynamic loaded videos
+        function videos (){
+            fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+            .then(res=>res.json())
+            .then(data=>{
+                dynamicVideos(data.videos);
+            })
+        }
+        const dynamicVideos = (videos)=>{
+            console.log(videos);
+            const dynamicVideosContainer = document.getElementById("dynamic-videos");
+            for(let video of videos){
+                console.log(video);
+                const div = document.createElement("div");
+                div.innerHTML = `
+    <div class=" shadow-sm">
+  <figure>
+    <img class="w-full h-[200px] object-cover"
+      src="${video.thumbnail}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+                
+                
+                
+                `;
 
+                dynamicVideosContainer.appendChild(div);
+            }
+        }
+    // 
 buttons();
 
 
@@ -30,6 +67,7 @@ buttons();
 
 
 
+// category_id: '1001', video_id: 'aaal', thumbnail: 'https://i.ibb.co/hdtZYbB/enchnting.jpg', title: 'Enchanted Harmonies', a
 
 
 
